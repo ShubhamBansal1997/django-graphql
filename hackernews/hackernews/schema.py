b9ecb93fa67ab0380858c04926cc7b56612a40c8
Shubham Bansal <shubhambansal17@hotmail.com>
2018-08-26 02:24:11 +0530

    Authorizaation and Authentication

--- hackernews/hackernews/schema.py
+++ hackernews/hackernews/schema.py
@@ -2,16 +2,22 @@
 # @Author: Shubham Bansal
 # @Date:   2018-08-26 01:07:08
 # @Last Modified by:   Shubham Bansal
-# @Last Modified time: 2018-08-26 01:22:57
+# @Last Modified time: 2018-08-26 02:12:40
 import graphene
+import graphql_jwt
 
 import links.schema
-
-class Query(links.schema.Query, graphene.ObjectType):
-  pass
+import users.schema
 
 
-class Mutation(links.schema.Mutation, graphene.ObjectType):
-  pass
+class Query(links.schema.Query, users.schema.Query, graphene.ObjectType):
+    pass
+
+
+class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType,):
+  token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+  verify_token = graphql_jwt.Verify.Field()
+  refresh_token = graphql_jwt.Refresh.Field()
+
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
