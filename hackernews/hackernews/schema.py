46a7df7924b03577dde7d948e35c469316b6243f
Shubham Bansal <shubhambansal17@hotmail.com>
2018-08-26 01:38:11 +0530

    Mutations

--- hackernews/hackernews/schema.py
+++ hackernews/hackernews/schema.py
@@ -2,7 +2,7 @@
 # @Author: Shubham Bansal
 # @Date:   2018-08-26 01:07:08
 # @Last Modified by:   Shubham Bansal
-# @Last Modified time: 2018-08-26 01:10:45
+# @Last Modified time: 2018-08-26 01:22:57
 import graphene
 
 import links.schema
@@ -11,4 +11,7 @@ class Query(links.schema.Query, graphene.ObjectType):
   pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(links.schema.Mutation, graphene.ObjectType):
+  pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -2,7 +2,7 @@
 # @Author: Shubham Bansal
 # @Date:   2018-08-26 01:03:45
 # @Last Modified by:   Shubham Bansal
-# @Last Modified time: 2018-08-26 01:12:02
+# @Last Modified time: 2018-08-26 01:26:21
 import graphene
 from graphene_django import DjangoObjectType
 
@@ -19,3 +19,30 @@ class Query(graphene.ObjectType):
 
   def resolve_links(self, info, **kwargs):
     return Links.objects.all()
+
+
+# Mutation
+class CreateLink(graphene.Mutation):
+  id = graphene.Int()
+  url = graphene.String()
+  description = graphene.String()
+
+  # arguments
+  class Arguments:
+    url = graphene.String()
+    description = graphene.String()
+
+  # the mutation function
+  def mutate(self, info, url, description):
+    link = Links(url=url, description=description)
+    link.save()
+
+    return CreateLink(
+      id=link.id,
+      url=link.url,
+      description=link.description,
+    )
+
+
+class Mutation(graphene.ObjectType):
+  create_link = CreateLink.Field()
